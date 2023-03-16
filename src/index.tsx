411a6b417ace9e8dad14737c037871e0a4265319
josubin47 <3_3_jimca@naver.com>
2023-03-17 00:16:46 +0900

    design: global css 작성

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    margin: 0;
+  }
+  #root {
+    height: 100vh;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
