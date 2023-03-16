15bf9ab76fe3475a52c2a0abc691ca1d61286679
josubin47 <3_3_jimca@naver.com>
2023-03-17 00:18:36 +0900

    feat: viewer 컴포넌트 추가

---
+++ src/components/Viewer/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+//import { LayoutProps } from './type';
+
+export default function Viewer() {
+  return <Section></Section>;
+}
+
+const Section = styled.section`
+  display: flex;
+  flex: 1;
+`;
