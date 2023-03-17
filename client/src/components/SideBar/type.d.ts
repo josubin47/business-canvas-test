9bac298e18019ccd32297ed213549ace07218735
josubin47 <3_3_jimca@naver.com>
2023-03-17 17:47:14 +0900

    feat: SideBar props 타입 정의

---
+++ client/src/components/SideBar/type.d.ts
@@ -0,0 +1,10 @@
+import { Resource } from 'recoil/resource';
+
+export interface SideBarProps {
+  resource: Resource[];
+  onAddResource: (param: Resource) => void;
+  onUpdateResource: (param: Resource) => void;
+  onDeleteResource: (id: number) => void;
+}
+
+export { SideBarProps };
