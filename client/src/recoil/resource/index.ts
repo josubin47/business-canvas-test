4f1cc6d3de63bc921954838012219b428a21e835
josubin47 <3_3_jimca@naver.com>
2023-03-17 17:48:22 +0900

    feat: id값으로 리소스를 필터링하는 selector 추가

--- client/src/recoil/resource/index.ts
+++ client/src/recoil/resource/index.ts
@@ -1,24 +1,37 @@
 import { atom, selectorFamily } from 'recoil';
 import { ResourceType } from 'types/common';
 
-interface ResourceState {
+export interface Resource {
   id: number;
   type: ResourceType;
   value: string;
 }
 
-export const resourceState = atom<ResourceState[]>({
-  key: 'resource',
+export const resourceState = atom<Resource[]>({
+  key: 'resourceState',
   default: [
     {
-      id: 0,
+      id: 1,
       type: 'URL',
       value: 'https://www.robinwieruch.de/react-libraries/',
     },
     {
-      id: 1,
+      id: 2,
       type: 'URL',
       value: 'https://typed.do/blog-kr/how-to-make-good-usability-product/',
     },
   ],
+});
+
+export const filteredResourceState = selectorFamily({
+  key: 'filteredResourceState',
+  get:
+    (id: number) =>
+    ({ get }) => {
+      const resource = get(resourceState);
+
+      return resource.find(item => {
+        item.id === id;
+      });
+    },
 });
