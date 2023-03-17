0df0a8a1118da85d05c0031c7a55c0f970fb4676
josubin47 <3_3_jimca@naver.com>
2023-03-17 18:35:14 +0900

    feat: 이미지 업로드를 위한 서버 구축

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/upload/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.use(cors());
+
+app.use(express.static("public"));
+
+// 파일 가져오기
+app.get("/image/:filename", function (req, res) {
+  const fileName = req.params.filename;
+  res.sendFile(path.join(__dirname, `/public/upload/${fileName}`));
+});
+
+// 업로드
+app.post("/image", upload.array("images", "10"), function (req, res, next) {
+  res.send("파일 업로드 성공");
+});
+
+app.listen(5000, function () {
+  console.log("Server listening on port 5000");
+});
