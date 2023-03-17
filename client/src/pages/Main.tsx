6a77e8595aa340799773f80da0c4ef46ead1ced7
josubin47 <3_3_jimca@naver.com>
2023-03-17 17:53:54 +0900

    feat: 전역 상태를 핸들링(추가, 수정, 삭제)하는 메소드 구현

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,11 +1,69 @@
+import { useRecoilState, useSetRecoilState, useRecoilValue } from 'recoil';
+import {
+  Resource,
+  resourceState,
+  filteredResourceState,
+} from 'recoil/resource';
 import styled from 'styled-components';
 import SideBar from '../components/SideBar';
 import Viewer from '../components/Viewer';
 
 export default function Main() {
+  const [resource, setResource] = useRecoilState<Resource[]>(resourceState);
+  const resourceHandler = useSetRecoilState(resourceState);
+
+  const filteredResource = (id: number) => {
+    return useRecoilValue(filteredResourceState(id));
+  };
+
+  // 리소스 추가
+  const handleAddResource = (param: Resource) => {
+    const obj: Resource = {
+      id: Math.max(...resource.map(item => item.id)) + 1,
+      type: param.type,
+      value: param.value,
+    };
+    resourceHandler([...resource, obj]);
+  };
+
+  // 리소스 수정
+  const handleUpdateResource = (param: Resource) => {
+    const filteredResourceItem = filteredResource(param.id);
+
+    if (filteredResourceItem === undefined) {
+      return;
+    }
+
+    resourceHandler(
+      resource.map(item => {
+        return item.id === filteredResourceItem.id
+          ? { ...item, value: filteredResourceItem.value }
+          : item;
+      }),
+    );
+  };
+
+  // 리소스 삭제
+  const handleDeleteResource = (id: number) => {
+    const filteredResourceItem = filteredResource(id);
+
+    if (filteredResourceItem === undefined) {
+      return;
+    }
+
+    resourceHandler(
+      resource.filter(item => item.id !== filteredResourceItem.id),
+    );
+  };
+
   return (
     <FlexContainer>
-      <SideBar />
+      <SideBar
+        resource={resource}
+        onAddResource={handleAddResource}
+        onDeleteResource={handleDeleteResource}
+        onUpdateResource={handleUpdateResource}
+      />
       <Viewer />
     </FlexContainer>
   );
