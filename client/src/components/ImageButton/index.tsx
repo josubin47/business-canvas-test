612be0de46a10e3de20e823010a3eb1b72296c2d
josubin47 <3_3_jimca@naver.com>
2023-03-17 18:35:32 +0900

    feat: 이미지 업로드 기능 구현

--- client/src/components/ImageButton/index.tsx
+++ client/src/components/ImageButton/index.tsx
@@ -1,8 +1,13 @@
-import React, { useRef } from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useRef } from 'react';
+import axios from 'axios';
 import { Button } from './style';
+import { ImageButtonProps } from './type';
+import { Resource } from 'recoil/resource';
 
-export default function ImageButton() {
+export default function ImageButton({
+  resource,
+  onAddResource,
+}: ImageButtonProps) {
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   const handleImageButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -12,8 +17,51 @@ export default function ImageButton() {
   };
 
   const handleImageInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // todo
+    const { files } = e.target;
+
+    const BASE_URL = 'http://localhost:5000';
+
+    if (!files) {
+      console.log('선택된 파일 없음');
+      return;
+    }
+
+    const resources: Resource[] = [];
+    let id = Math.max(...resource.map(item => item.id)) + 1;
+
+    const formData = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      const obj: Resource = {
+        id: id,
+        type: 'IMAGE',
+        value: files[i].name,
+      };
+      resources.push(obj);
+
+      //id 값으로 파일 저장
+      formData.append(
+        'images',
+        files[i],
+        String(obj.id) + '.' + files[i].name.split('.').pop(),
+      );
+
+      id++;
+    }
+
+    axios
+      .post(BASE_URL + '/image', formData)
+      .then(res => {
+        console.log(res.data);
+        onAddResource(resources);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
+
+  useEffect(() => {
+    console.log(resource);
+  }, [resource]);
 
   return (
     <>
