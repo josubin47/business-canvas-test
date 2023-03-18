2993eb66ba3af4db94adbe3ff3cbfb3d77453bf9
josubin47 <3_3_jimca@naver.com>
2023-03-18 11:52:58 +0900

    feat: 리스트 목록 수정, 삭제 기능 구현

--- client/src/components/ListItem/index.tsx
+++ client/src/components/ListItem/index.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { TypedIcon } from 'typed-design-system';
-import { ResourceType } from 'types/common';
-import { Item, TextField, Button } from './style';
+import { Item, TextField, Button, ButtonContainer } from './style';
 import { ItemProps } from './type';
 
 export default function ListItem({
@@ -11,19 +9,67 @@ export default function ListItem({
   onSelectedResource,
   isSelected,
 }: ItemProps) {
-  const [value, setValue] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [isEditing, setIsEditing] = useState<boolean>(false);
 
-  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleItemClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
     onSelectedResource(resource.id);
   };
 
+  const handleUpdateItem = () => {
+    onUpdateResource({ ...resource, name: resource.name });
+  };
+
+  const handleDeleteItem = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    onDeleteResource(resource.id);
+  };
+
+  // 수정 핸들러
+  const handleEditingButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setIsEditing(true);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation();
+    setName(e.target.value);
+  };
+
+  const handleInputBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    e.stopPropagation();
+    setIsEditing(false);
+    handleUpdateItem();
+  };
+
   useEffect(() => {
-    setValue(resource.value);
-  }, [value]);
+    setName(resource.name);
+  }, []);
 
   return (
-    <Item isSelected={isSelected} onClick={handleClick}>
-      <TextField>{value}</TextField>
+    <Item isSelected={isSelected} onClick={handleItemClick}>
+      <TextField>
+        {isEditing ? (
+          <input
+            type="text"
+            value={name}
+            onChange={handleInputChange}
+            onBlur={handleInputBlur}
+            onClick={e => e.stopPropagation()}
+          />
+        ) : (
+          <span>{name}</span>
+        )}
+      </TextField>
+      <ButtonContainer>
+        <Button type="button" onClick={handleEditingButtonClick}>
+          수정
+        </Button>
+        <Button type="button" onClick={handleDeleteItem}>
+          삭제
+        </Button>
+      </ButtonContainer>
     </Item>
   );
 }
