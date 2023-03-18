3730de95d0c5c22d12c80104b8794cc96974a8aa
josubin47 <3_3_jimca@naver.com>
2023-03-18 12:00:08 +0900

    remove: 사용하지 않는 파일 삭제

--- client/src/hooks/useFilteredResource.ts
+++
@@ -1,6 +0,0 @@
-import { useRecoilValue } from 'recoil';
-import { filteredResourceState } from 'recoil/resource';
-
-export default function useFilteredResource(id: number) {
-  return useRecoilValue(filteredResourceState(id));
-}
