02dfc3f8ba51a00ac7121703bf0e8b7bb7b7b276
josubin47 <3_3_jimca@naver.com>
2023-03-18 13:17:13 +0900

    style: url 뷰어 사이즈 수정

--- client/src/components/URLViewer/index.tsx
+++ client/src/components/URLViewer/index.tsx
@@ -2,14 +2,12 @@ import { URLViewerProps } from './type';
 
 export default function URLViewer({ url }: URLViewerProps) {
   return (
-    <div>
-      <iframe
-        src={url}
-        title="URL Viewer"
-        width="800px"
-        height="600px"
-        allowFullScreen
-      />
-    </div>
+    <iframe
+      src={url}
+      title="URL Viewer"
+      width="100%"
+      height="100%"
+      allowFullScreen
+    />
   );
 }
